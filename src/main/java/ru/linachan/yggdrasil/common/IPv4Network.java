9db046e68a8a784ec5c2a27c95f0c3b71a64b2e1
Alexey Deryugin <aderyugin@mirantis.com>
2016-03-21 19:55:54 +0300

    Add IPv4Network calculator
    Change-Id: Icdc5a84e41bf1f47c91af81f3da75a77105b4e6a

---
+++ src/main/java/ru/linachan/yggdrasil/common/IPv4Network.java
@@ -0,0 +1,78 @@
+package ru.linachan.yggdrasil.common;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IPv4Network {
+
+    private Long networkAddress;
+    private Integer netMaskBits;
+
+    private Random randomGenerator = new Random();
+
+    private final Pattern NET_DEFINITION_PATTERN = Pattern.compile(
+        "^(?<address>([01]?\\d?\\d|2[0-4]\\d|25[0-5])\\.([01]?\\d?\\d|2[0-4]\\d|25[0-5])\\.([01]?\\d?\\d|2[0-4]\\d|25[0-5])\\.([01]?\\d?\\d|2[0-4]\\d|25[0-5]))/(?<cidr>(3[0-2]|[0-2]?\\d))$"
+    );
+
+    public IPv4Network(String networkDefinition) {
+        Matcher networkDefinitionMatcher = NET_DEFINITION_PATTERN.matcher(networkDefinition);
+        if (networkDefinitionMatcher.matches()) {
+            networkAddress = ip2long(networkDefinitionMatcher.group("address"));
+            netMaskBits = Integer.valueOf(networkDefinitionMatcher.group("cidr"));
+        } else {
+            throw new IllegalArgumentException("Invalid network definition");
+        }
+    }
+
+    private static String long2ip(Long number) {
+        return ((number >> 24) & 0xFF) + "." + ((number >> 16) & 0xFF) + "." + ((number >> 8) & 0xFF) + "." + (number & 0xFF);
+    }
+
+    private static Long ip2long(String address) {
+        String[] octets = address.split("\\.");
+        long number = 0;
+
+        for (int i = 0; i < octets.length; i++) {
+            number += ((Integer.parseInt(octets[i]) % 256 * Math.pow(256, 3 - i)));
+        }
+
+        return number;
+    }
+
+    public IPv4Network(Inet4Address networkAddress, Integer netMaskBits) {
+        this.networkAddress = ip2long(networkAddress.getHostAddress());
+        this.netMaskBits = netMaskBits;
+    }
+
+    public InetAddress getRandomAddress() {
+        long addressOffset = randomGenerator.nextLong() % Math.round(Math.pow(2, 32 - netMaskBits));
+
+        try {
+            return InetAddress.getByName(long2ip(networkAddress + addressOffset));
+        } catch (UnknownHostException e) {
+            return null;
+        }
+    }
+
+    public boolean inRange(Inet4Address inetAddress) {
+        return inRange(inetAddress.getHostAddress());
+    }
+
+    public boolean inRange(String inetAddress) {
+        long inetAddressNumber = ip2long(inetAddress);
+        return (networkAddress <= inetAddressNumber)&&(inetAddressNumber <= networkAddress + Math.round(Math.pow(2, 32 - netMaskBits)) - 1);
+    }
+
+    public Inet4Address getNetworkAddress() throws UnknownHostException {
+        return (Inet4Address) InetAddress.getByName(long2ip(networkAddress));
+    }
+
+    public Integer getNetMask() {
+        return netMaskBits;
+    }
+}
+
---
+++ src/main/test/ru/linachan/yggdrasil/common/IPv4NetworkTest.java
@@ -0,0 +1,50 @@
+package ru.linachan.yggdrasil.common;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import ru.linachan.yggdrasil.YggdrasilTestBase;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.*;
+
+public class IPv4NetworkTest extends YggdrasilTestBase {
+
+    @Test
+    public void testAcceptValidNetwork() throws UnknownHostException {
+        IPv4Network networkFromString = new IPv4Network("127.0.0.0/8");
+        IPv4Network networkFromInetAddress = new IPv4Network((Inet4Address) Inet4Address.getByName("192.168.0.0"), 24);
+
+        Assert.assertEquals("127.0.0.0", networkFromString.getNetworkAddress().getHostAddress());
+        Assert.assertEquals(Integer.valueOf(8), networkFromString.getNetMask());
+
+        Assert.assertEquals("192.168.0.0", networkFromInetAddress.getNetworkAddress().getHostAddress());
+        Assert.assertEquals(Integer.valueOf(24), networkFromInetAddress.getNetMask());
+    }
+
+    @Test
+    public void testInvalidNetwork() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Invalid network definition");
+        new IPv4Network("256.0.0.255/10");
+    }
+
+    @Test
+    public void testRangeCheck() {
+        IPv4Network networkRange = new IPv4Network("172.16.0.0/16");
+
+        Assert.assertTrue(networkRange.inRange("172.16.124.128"));
+        Assert.assertFalse(networkRange.inRange("192.168.0.1"));
+    }
+
+    @Test
+    public void testRandomAddressGenerator() {
+        IPv4Network networkRange = new IPv4Network("172.16.0.0/16");
+
+        InetAddress randomAddress = networkRange.getRandomAddress();
+
+        Assert.assertTrue(networkRange.inRange((Inet4Address) randomAddress));
+    }
+}
