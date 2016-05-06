90a161a5c21c2634c8f134054a0ec50fc297914b
Alexey Deryugin <aderyugin@mirantis.com>
2016-05-06 19:17:54 +0300

    Make  RPCClient asynchronous
    Change-Id: I4e70d1f56f4eab50eb05e698a349612459568305

--- src/main/java/ru/linachan/rpc/RPCClient.java
+++ src/main/java/ru/linachan/rpc/RPCClient.java
@@ -4,16 +4,26 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.QueueingConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-public class RPCClient {
+public class RPCClient implements Runnable {
 
     private Connection connection;
     private Channel channel;
     private String requestQueueName;
     private String replyQueueName;
     private QueueingConsumer consumer;
+    private Boolean isRunning = true;
+
+    private Map<String, RPCCallback> callbackMap = new HashMap<>();
+
+    private static Logger logger = LoggerFactory.getLogger(RPCClient.class);
 
     public RPCClient(Connection rpcConnection, String rpcQueue) throws IOException {
         connection = rpcConnection;
@@ -26,30 +36,36 @@ public class RPCClient {
         channel.basicConsume(replyQueueName, true, consumer);
     }
 
-    public String call(String message) throws Exception {
-        String response;
-        String corrId = java.util.UUID.randomUUID().toString();
+    public void call(String message, RPCCallback callback) throws IOException, InterruptedException {
+        String corrId = UUID.randomUUID().toString();
 
         AMQP.BasicProperties props = new AMQP.BasicProperties
-                .Builder()
-                .correlationId(corrId)
-                .replyTo(replyQueueName)
-                .build();
+            .Builder()
+            .correlationId(corrId)
+            .replyTo(replyQueueName)
+            .build();
 
         channel.basicPublish("", requestQueueName, props, message.getBytes());
 
-        while (true) {
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
-                response = new String(delivery.getBody());
-                break;
-            }
-        }
-
-        return response;
+        callbackMap.put(corrId, callback);
     }
 
-    public void close() throws Exception {
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                if (callbackMap.containsKey(delivery.getProperties().getCorrelationId())) {
+                    callbackMap.get(delivery.getProperties().getCorrelationId()).callback(new String(delivery.getBody()));
+                }
+            } catch (InterruptedException e) {
+                logger.error("Unable to handle RPC message: {}", e.getMessage());
+            }
+        }
+    }
+
+    public void shutdown() throws IOException {
         connection.close();
+        isRunning = false;
     }
 }
