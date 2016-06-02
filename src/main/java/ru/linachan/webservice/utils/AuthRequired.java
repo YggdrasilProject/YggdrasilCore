68742ab76aea923664fc7e4cb4385f795178d0bc
Alexey Deryugin <aderyugin@mirantis.com>
2016-06-02 22:21:41 +0300

    TCPServer and WebService plugins from YggdrasilCore source tree
    Change-Id: Ic2d84b240e4d7d0009ca48e16de0179583ee7496

--- src/main/java/ru/linachan/webservice/utils/AuthRequired.java
+++
@@ -1,9 +0,0 @@
-package ru.linachan.webservice.utils;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AuthRequired {
-    String value() default "Yggdrasil WebService";
-}
