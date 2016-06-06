4ccd409cbe7a029677888c7de2afedf80de08617
Alexey Deryugin <aderyugin@mirantis.com>
2016-06-06 17:11:14 +0300

    Add MySQL Plugin
    Change-Id: I236ae7bd8d82c5e1f0abcf77950ebf77deca4520

---
+++ src/main/java/ru/linachan/mysql/Table.java
@@ -0,0 +1,10 @@
+package ru.linachan.mysql;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table {
+
+    String name();
+}
